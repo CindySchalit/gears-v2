2a7f5bda24d4c5e1670c675dc8dcaebd94444fc3
CindySchalit <cindyschalit@gmail.com>
2017-06-11 11:39:35 -0700

    Chore(App): add bad fish image

--- app/constants.js
+++ app/constants.js
@@ -1,2 +1,3 @@
 export const SET_SPEED = 'SET_SPEED'
 export const SET_PEDAL_PRESSURE = 'SET_PEDAL_PRESSURE'
+export const SET_RPM = 'SET_RPM'
--- app/containers/index.js
+++ app/containers/index.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react'
 import SpeedSliderContainer from './SpeedSliderContainer'
 import PedalPressureSliderContainer from './PedalPressureSliderContainer'
 
-const pressureTypes = {
-
-}
-
 export default class AllSliders extends Component {
   render() {
     return (
       <div>
         <h1>Bad Fish Gear Shifter</h1>
+        <center>
+          <img
+            src="http://drshaunayoung.com/wp-content/uploads/2013/07/mean-fish.png"
+            height="100"
+            width="100"
+          />
+        </center>
         <PedalPressureSliderContainer />
         <SpeedSliderContainer />
       </div>
