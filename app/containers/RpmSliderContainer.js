561c66f878fef6a262facb3bd6f49fdd00914204
CindySchalit <cindyschalit@gmail.com>
2017-06-14 05:51:43 -0700

    Feat(RPM): render rpm from Rpm Component

--- app/containers/RpmSliderContainer.js
+++ app/containers/RpmSliderContainer.js
@@ -4,49 +4,18 @@ import { setRpm } from '../action-creators/rpm';
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => {
+  let pedalPressureInRpm = null
+  let speedInRpm = null
+
+  if (state) {
+    pedalPressureInRpm = state.pedalPressure
+    speedInRpm = state.speed
+  }
+
   return {
-    speed: state.speed,
-    pedalPressure: state.pedalPressure,
-    rpm: state.rpm,
+    pedalPressure: pedalPressureInRpm,
+    speed: speedInRpm,
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setRpmDispatcher: (pedalPressure, speed) => {
-      let rpm = 1000
-
-      // no pressure on pedal
-      if (pedalPressure === 0) {
-        rpm = 0
-        return dispatch(setRpm(rpm))
-      }
-
-      // light pressure on pedal
-      if (pedalPressure === 1) {
-        if (speed < 10) {
-          rpm = speed * 228
-          return dispatch(setRpm(rpm))
-        }
-
-        if (speed < 25) {
-          rpm = speed * 133
-          return dispatch(setRpm(rpm))
-        }
-
-        if (speed < 46) {
-          rpm = speed * 94
-          return dispatch(setRpm(rpm))
-        }
-
-        if (speed < 74)  {
-          rpm = speed * 72
-        }
-      }
-
-      // heavy pressure on pedal
-    }
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(RpmSlider)
+export default connect(mapStateToProps)(RpmSlider)
