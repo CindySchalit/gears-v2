96d84058116f4061856c758f1609acccf693862d
CindySchalit <cindyschalit@gmail.com>
2017-06-14 14:06:06 -0700

    Feat(App): update styling

--- app/containers/index.js
+++ app/containers/index.js
@@ -6,15 +6,16 @@ import RpmSliderContainer from './RpmSliderContainer'
 export default class AllSliders extends Component {
   render() {
     return (
-      <div>
-        <center>
+      <div id="App">
+        <div id="App-Header">
           <h1>Bad Fish Gear Shifter</h1>
           <img
             src="http://drshaunayoung.com/wp-content/uploads/2013/07/mean-fish.png"
             height="100"
             width="100"
+            id="fish"
           />
-        </center>
+        </div>
         <div>
           <PedalPressureSliderContainer />
           <SpeedSliderContainer />
